5eb7ba0fbb34b085f80f027636be7b4f9de84f3b
renatomdesouza <renatomdesouza@yahoo.com.br>
2024-04-16 20:25:39 -0300

    Inicio da implementacao do Command Pattern

---
+++ src/main/java/br/com/erprms/serviceApplication/personService/command/Command.java
@@ -0,0 +1,7 @@
+package br.com.erprms.serviceApplication.personService.command;
+
+import br.com.erprms.serviceApplication.personService.SpecifiedQualificationConstants;
+
+public interface Command extends SpecifiedQualificationConstants{
+	void execute();
+}
